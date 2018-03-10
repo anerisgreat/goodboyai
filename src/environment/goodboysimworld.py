1a904482b2ec92bded76d68167230f692f7d3b92
Aner Zakovitch <anerisgreat@gmail.com>
2018-03-10 23:48:12 +0200

    Added a ball

--- src/environment/goodboysimworld.py
+++ src/environment/goodboysimworld.py
@@ -13,13 +13,29 @@ class simworld(object):
 		self.height = 600
 
 		self.space = pymunk.Space()
-		self.space.gravity = (0.0, -900.0)
+		self.space.gravity = (0.0, -10.0)
 
 	def load_world(self):
 		static_body = self.space.static_body
-		self.floor = pymunk.Segment(static_body, (0, 0), (self.width, self.height), 0)
+		self.floor = pymunk.Segment(static_body, (0, 10), (self.width, 10), 0)
 
 		self.space.add(self.floor)
+
+		self.load_test_ball()
+
+	def load_test_ball(self):
+		mass = 1
+		radius = 14
+		inertia = pymunk.moment_for_circle(mass, 0, radius, (0, 0))
+		body = pymunk.Body(mass, inertia)
+		x = random.randint(120, 380)
+		body.position = (x, 550)
+		shape = pymunk.Circle(body, radius, (0, 0))
+
+		self.test_body = body
+
+		self.space.add(body, shape)
+
 
 	def init_screen(self):
 		pygame.init()
@@ -35,6 +51,8 @@ class simworld(object):
 					self.running = False
 				if event.type == KEYDOWN and event.key == K_ESCAPE:
 					self.running = False
+				if event.type == KEYDOWN and event.key == K_UP:
+					self.test_body.cpv(0, -10)
 
 			self.screen.fill(THECOLORS["white"])
 
